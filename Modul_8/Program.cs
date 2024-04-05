f135c7b248efae13faa34278df1a53ef483150ff
Albina <kamalieva.albish@mail.ru>
2024-04-05 18:54:40 +0300

    Добавьте файлы проекта.

---
+++ Modul_8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Modul_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //получим системные диски
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            //пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives) 
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }
+        }
+    }
+
+    public class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace) 
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+        public string Name { get; set; }
+        public long TotalSpace { get; set; }
+        public long FreeSpace { get; set; }
+
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+        public class Folder
+        {
+            public List<string> Files { get; set; } = new List<string>();
+        }
+    }
+
+
+}
